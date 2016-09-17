cd21e31223da63a0331913be2b490aa754712439
User <User@CQ.local>
2016-09-17 13:49:33 -0600

    added js

---
+++ app/script/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $(function fixHead() {
+      var headerShow = $(".header");
+      $(window).scroll(function() {
+          var scroll = $(window).scrollTop();
+
+          if (scroll >= 3) {
+              headerShow.removeClass("header").addClass("showing");
+          } else {
+              headerShow.removeClass("showing").addClass("header");
+          }
+          if (scroll > 4){
+            $('.logo').hide();
+          }
+          else {
+            $('.logo').show();
+          }
+      });
+  });
+
+
+
+});
