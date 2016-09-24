6fc5438ea82c41803638ca03671a5a19c9326d0b
User <User@CQ.local>
2016-09-23 22:03:13 -0600

    loops and loops

--- app/js/infinite-rotator.js
+++ app/js/infinite-rotator.js
@@ -3,28 +3,17 @@ $(document).ready(function(){
 
     $(window).load(function() {
 
-var InfiniteRotator =
-{
-init: function()
-{
+var InfiniteRotator = {
 
-var initialFadeIn = 1000;
+init: function(){
 
-
+var initialFadeIn = 2000;
 var itemInterval = 5000;
-
-
 var fadeTime = 2500;
-
-
 var numberOfItems = $('.rotating-item').length;
-
-
 var currentItem = 0;
 
-
 $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
-
 
 var infiniteLoop = setInterval(function(){
     $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
@@ -33,13 +22,37 @@ var infiniteLoop = setInterval(function(){
         currentItem = 0;
     }else{
         currentItem++;
-    }
+          }
     $('.rotating-item').eq(currentItem).fadeIn(fadeTime);
 
-}, itemInterval);
-}
+     }, itemInterval);
+   }
 };
 
+var InfiniteRotatorBox = {
+
+init: function(){
+
+var initialFadeInBox = 2000;
+var itemIntervalBox = 5000;
+var fadeTimeBox = 4000;
+var numberOfItemsBox = $('.box-move').length;
+var currentItemBox = 0;
+
+$('.box-move').eq(currentItemBox).fadeOut(initialFadeInBox);
+
+var infiniteLoop = setInterval(function(){
+    $('.box-move').eq(currentItemBox).fadeOut(fadeTimeBox);
+
+    if(currentItemBox == numberOfItemsBox -1){
+        currentItemBox = 0;
+    }else{
+        $('.box-move').eq(currentItemBox++).fadeIn(fadeTimeBox);
+      }
+  }, itemIntervalBox);
+   }
+};
+InfiniteRotatorBox.init();
 InfiniteRotator.init();
 
 });
@@ -67,6 +80,7 @@ InfiniteRotator.init();
           else {
             logos.fadeIn(1500);
           }
-        })
-      })
+        });
+      });
+
     });
