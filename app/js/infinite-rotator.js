ac6c5d3e6ba9271e0c3382c35aff529a6dc0c072
User <User@CQ.local>
2016-09-24 14:11:21 -0600

    loop almost done

--- app/js/infinite-rotator.js
+++ app/js/infinite-rotator.js
@@ -8,17 +8,18 @@ var InfiniteRotator = {
 init: function(){
 
 var initialFadeIn = 2000;
-var itemInterval = 5000;
-var fadeTime = 2500;
+var itemInterval = 4000;
+var fadeTime = 4000;
 var numberOfItems = $('.rotating-item').length;
 var currentItem = 0;
 
 $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
 
 var infiniteLoop = setInterval(function(){
+    console.log('pic')
     $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
 
-    if(currentItem == numberOfItems -1){
+    if(currentItem === numberOfItems -1){
         currentItem = 0;
     }else{
         currentItem++;
@@ -27,31 +28,33 @@ var infiniteLoop = setInterval(function(){
 
      }, itemInterval);
    }
-};
-
+}
 var InfiniteRotatorBox = {
 
 init: function(){
 
 var initialFadeInBox = 2000;
-var itemIntervalBox = 5000;
+var itemIntervalBox = 1000;
 var fadeTimeBox = 4000;
 var numberOfItemsBox = $('.box-move').length;
 var currentItemBox = 0;
 
-$('.box-move').eq(currentItemBox).fadeOut(initialFadeInBox);
+  $('.box-move').eq(0).css('opacity', 0.2);
 
 var infiniteLoop = setInterval(function(){
-    $('.box-move').eq(currentItemBox).fadeOut(fadeTimeBox);
-
-    if(currentItemBox == numberOfItemsBox -1){
+  console.log(currentItemBox);
+    $('.box-move').eq(currentItemBox + 1).css('opacity', 0.2);
+    $('.box-move').eq(currentItemBox).css('opacity', 1);
+    if (currentItemBox === numberOfItemsBox -1){
         currentItemBox = 0;
+        $('.box-move').eq(0).css('opacity', 0.2);
     }else{
-        $('.box-move').eq(currentItemBox++).fadeIn(fadeTimeBox);
-      }
-  }, itemIntervalBox);
+        currentItemBox++;
+          }
+
+     }, itemIntervalBox);
    }
-};
+}
 InfiniteRotatorBox.init();
 InfiniteRotator.init();
 
@@ -80,7 +83,6 @@ InfiniteRotator.init();
           else {
             logos.fadeIn(1500);
           }
-        });
+        })
       });
-
     });
